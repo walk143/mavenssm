ee06dd9594b8102a05a99cf00b1c0fbd842eda8f
liuwangyang <liuwangyang@inspur.com>
2020-01-20 17:18:03 +0800

    maven相关-部分修改
    Signed-off-by: liuwangyang <liuwangyang@inspur.com>

--- src/main/java/com/sloera/mng/core/error/FreemarkerException.java
+++ src/main/java/com/sloera/mng/core/error/FreemarkerException.java
@@ -3,23 +3,22 @@ package com.sloera.mng.core.error;
 import freemarker.core.Environment;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
-import org.slf4j.Logger;
+import org.apache.log4j.Logger;
+
 import java.io.Writer;
 
-import org.slf4j.LoggerFactory;
-
 public class FreemarkerException implements TemplateExceptionHandler {
-    private static final Logger log4j = LoggerFactory.getLogger(FreemarkerException.class);
+    private static final Logger log4j = Logger.getLogger(FreemarkerException.class);
     public FreemarkerException(){};
 
     @Override
     public void handleTemplateException(TemplateException e, Environment environment, Writer writer) throws TemplateException {
-        if(log4j.isWarnEnabled()){
+        if(log4j.isInfoEnabled()){
             
         }
-        if (log4j.isWarnEnabled()) {
-            log4j.warn("[Freemarker Error: " + e.getMessage() + "]");
-        }
+        //if (log4j.isWarnEnabled()) {
+        //    log4j.warn("[Freemarker Error: " + e.getMessage() + "]");
+        //}
 
         if (log4j.isInfoEnabled()) {
             log4j.info("[Freemarker Error: " + e.getMessage() + "]");
